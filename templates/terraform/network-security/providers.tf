9e703d73b80dad74a9740f25a32bff8a489d6db2
MHC <mworwell@hotmail.com>
2025-04-07 19:42:52 -0700

    Create providers.tf

---
+++ templates/terraform/network-security/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Or your preferred up-to-date version
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  features {}
+  # Recommend using authentication methods like Azure CLI, Service Principal with OIDC, or Managed Identity
+}
